ad7dbaa6b90800c2843d0aa0ba0e118b8b8e4c1b
Curtis Allen <curtis.allen@robotsandpencils.com>
2016-09-22 17:34:48 -0600

    provide loggin option if verify token is incorrect

--- src/receiver/middleware/verify-token.js
+++ src/receiver/middleware/verify-token.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = (token) => {
+module.exports = (token, shouldLog) => {
   return function verifyTokenMiddleware (req, res, next) {
     // If token isn't set, we're not verifying
     if (!token) {
@@ -12,6 +12,8 @@ module.exports = (token) => {
 
     // test verify token
     if (token !== verifyToken) {
+      // log when there's an inviald verify token
+      if (shouldLog) console.log('Invalid verify token')
       res.status(403).send('Invalid token')
       return
     }
