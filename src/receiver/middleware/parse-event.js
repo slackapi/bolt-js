ca08f117f06c586b5adfb7e3ca27b41d0b141882
Simon Tam <simonsayscode@users.noreply.github.com>
2017-04-29 17:26:59 -0400

    Fixing issue #84 for support for user object in event payload as opposed to a user id string. (#85)

--- src/receiver/middleware/parse-event.js
+++ src/receiver/middleware/parse-event.js
@@ -20,13 +20,20 @@ module.exports = () => {
       let body = req.body || {}
       let event = body.event || {}
       let channelId = event.channel || (event.item && event.item.channel)
+      let userId
+
+      if (typeof event.user === 'string') {
+        userId = event.user
+      } else if (typeof event.user === 'object') {
+        userId = event.user.id
+      }
 
       req.slapp = {
         type: 'event',
         body: body,
         meta: {
           verify_token: body.token,
-          user_id: event.user,
+          user_id: userId,
           bot_id: event.bot_id,
           channel_id: channelId,
           team_id: body.team_id
