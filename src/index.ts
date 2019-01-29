4f052dbc0b3c66c64328985ed16ce2053ab1c7e6
Ankur Oberoi <aoberoi@gmail.com>
2019-01-29 14:28:36 -0800

    typechecker happy with index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-'use strict'
-
-const Slapp = require('./slapp')
+import Slapp, { SlappOptions } from './Slapp';
 
 /**
  * Create a new Slapp, accepts an options object
  *
  * Parameters
- * - `opts.verify_token` Slack Veryify token to validate authenticity of requests coming from Slack
+ * - `opts.verify_token` Slack Verify token to validate authenticity of requests coming from Slack
  * - `opts.signing_secret` Slack signing secret to check/verify the signature of requests coming from Slack
  * - `opts.signing_version` Slack signing version string, defaults to 'v0'
  * - `opts.convo_store` Implementation of ConversationStore, defaults to memory
@@ -14,7 +12,8 @@ const Slapp = require('./slapp')
  * - `opts.log` defaults to `true`, `false` to disable logging
  * - `opts.logger` Implementation of a logger, defaults to built-in Slapp command line logger.
  * - `opts.colors` defaults to `process.stdout.isTTY`, `true` to enable colors in logging
- * - `opts.ignoreSelf` defaults to `true`, `true` to automatically ignore any messages from yourself. This flag requires the context to set `meta.app_bot_id` with the Slack App's users.profile.bot_id.
+ * - `opts.ignoreSelf` defaults to `true`, `true` to automatically ignore any messages from yourself. This flag requires
+ *    the context to set `meta.app_bot_id` with the Slack App's users.profile.bot_id.
  * - `opts.ignoreBots` defaults to `false`, `true` to ignore any messages from bot users automatically
  *
  * Example
@@ -28,18 +27,12 @@ const Slapp = require('./slapp')
  *       context: BeepBoopContext(),
  *       convo_store: BeepBoopConvoStore({ debug: true })
  *     })
- *
- *
- * @param {Object} opts
- * @returns {Object} Slapp
- * @function slapp
- * @alias slapp
  */
 
-function factory (opts) {
-  let app = new Slapp(opts)
+function factory(opts: SlappOptions): Slapp {
+  const app = new Slapp(opts);
 
-  return app.init()
+  return app.init();
 }
 
-module.exports = factory
+module.exports = factory;
