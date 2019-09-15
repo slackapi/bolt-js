861d3904b6167bbeb2b04f55631a7db0f87f98d7
43081j <43081j@users.noreply.github.com>
2019-09-15 11:03:10 +0100

    remove redundant types

--- types/please-upgrade-node.d.ts
+++
@@ -1,8 +0,0 @@
-export = pleaseUpgradeNode;
-
-interface Options {
-  message?: (requiredVersion: string) => unknown;
-  exitCode?: number;
-}
-
-declare function pleaseUpgradeNode(packageJson: any, options?: Options): void;
