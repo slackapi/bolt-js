6ea7a5a0f800a87fa48a3214c5b9fc349489354a
Kazuhiro Sera <seratch@gmail.com>
2019-12-24 15:28:00 +0900

    Fix #168 Give listeners and middleware access to the logger

--- src/types/events/index.ts
+++ src/types/events/index.ts
@@ -2,11 +2,13 @@ export * from './base-events';
 import { SlackEvent, BasicSlackEvent } from './base-events';
 import { StringIndexed } from '../helpers';
 import { SayFn } from '../utilities';
+import { Logger } from '@slack/logger';
 
 /**
  * Arguments which listeners and middleware receive to process an event from Slack's Events API.
  */
 export interface SlackEventMiddlewareArgs<EventType extends string = string> {
+  logger: Logger;
   payload: EventFromType<EventType>;
   event: this['payload'];
   message: EventType extends 'message' ? this['payload'] : never;
