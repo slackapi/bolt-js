29334055136d8dadf867bfa60a11409f31afb11e
Kian Attari <kattari@slack-corp.com>
2020-06-26 17:25:20 -0400

    added test lint + cleaned up

--- .eslintrc.test.js
+++ .eslintrc.test.js
@@ -1,13 +1,26 @@
 module.exports = {
-  // FIXME 
-  "extends": [".eslintrc.js"],
-  "overrides": [{
-    "files": ["**/*.spec.ts", "src/test-helpers.ts"],
-    "env":{
+    "extends": [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    "env": {
+        "commonjs": true,
         "mocha": true
     },
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
-      "project": "tsconfig.test.json"
+        "project": "tsconfig.test.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+        "prefer-rest-params": "off"
     }
-  }]
+
 };
