ed5ee87627abb4df413ea061b138f1c721ccb4e9
Shane DeWael <sdewael@slack-corp.com>
2021-02-08 13:52:29 -0800

    update message types

--- src/types/events/message-events.ts
+++ src/types/events/message-events.ts
@@ -13,16 +13,22 @@ export type MessageEvent =
 export interface GenericMessageEvent {
   type: 'message';
   subtype: undefined;
+  event_ts: string;
+  team: string;
   channel: string;
   user: string;
   text?: string;
   ts: string;
+  thread_ts?: string;
+  channel_type: channelTypes;
   attachments?: MessageAttachment[];
   blocks?: (KnownBlock | Block)[];
   edited?: {
     user: string;
     ts: string;
   };
+  client_msg_id?: string;
+  parent_user_id?: string;
 
   // TODO: optional types that maybe should flow into other subtypes?
   is_starred?: boolean;
@@ -37,8 +43,9 @@ export interface GenericMessageEvent {
 export interface BotMessageEvent {
   type: 'message';
   subtype: 'bot_message';
+  event_ts: string;
   channel: string;
-  channel_type: string;
+  channel_type: channelTypes;
   ts: string;
   text: string;
   bot_id: string;
@@ -62,8 +69,9 @@ export interface BotMessageEvent {
 export interface EKMAccessDeniedMessageEvent {
   type: 'message';
   subtype: 'ekm_access_denied';
+  event_ts: string;
   channel: string;
-  channel_type: string;
+  channel_type: channelTypes;
   ts: string;
   text: string; // This will not have any meaningful content within
   user: 'UREVOKEDU';
@@ -72,8 +80,9 @@ export interface EKMAccessDeniedMessageEvent {
 export interface MeMessageEvent {
   type: 'message';
   subtype: 'me_message';
+  event_ts: string;
   channel: string;
-  channel_type: string;
+  channel_type: channelTypes;
   user: string;
   text: string;
   ts: string;
@@ -82,28 +91,34 @@ export interface MeMessageEvent {
 export interface MessageChangedEvent {
   type: 'message';
   subtype: 'message_changed';
+  event_ts: string;
   hidden: true;
   channel: string;
-  channel_type: string;
+  channel_type: channelTypes;
   ts: string;
   message: MessageEvent;
+  previous_message: MessageEvent;
 }
 
 export interface MessageDeletedEvent {
   type: 'message';
   subtype: 'message_deleted';
+  event_ts: string;
   hidden: true;
   channel: string;
+  channel_type: channelTypes;
   ts: string;
   deleted_ts: string;
+  previous_message: MessageEvent;
 }
 
 export interface MessageRepliedEvent {
   type: 'message';
   subtype: 'message_replied';
+  event_ts: string;
   hidden: true;
   channel: string;
-  event_ts: string;
+  channel_type: channelTypes;
   ts: string;
   message: MessageEvent & {
     // TODO: should this be the union of all message events with type 'message'?
@@ -118,7 +133,9 @@ export interface MessageRepliedEvent {
 export interface ThreadBroadcastMessageEvent {
   type: 'message';
   subtype: 'thread_broadcast';
+  event_ts: string;
   text: string;
+  blocks?: (KnownBlock | Block)[];
   user: string;
   ts: string;
   thread_ts?: string;
@@ -129,8 +146,10 @@ export interface ThreadBroadcastMessageEvent {
     latest_reply: string;
     reply_users: string[];
   };
-  client_msg_id: string;
+  client_msg_id?: string;
   channel: string;
-  event_ts: string;
-  channel_type: string;
+  channel_type: channelTypes;
 }
+
+// Include string in channel_type to prevent future backwards-incompatible changes
+export type channelTypes = 'channel' | 'group' | 'im' | 'mpim' | 'app_home' | string;
