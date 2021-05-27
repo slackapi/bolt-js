ca969267584255bd2ada3825ec4ceaf4ec8560c1
Jordan Loeser <loeserjordan@gmail.com>
2021-05-27 11:06:18 -0400

    Apply suggestions from code review
    Co-authored-by: Kazuhiro Sera <seratch@gmail.com>

--- src/types/events/base-events.ts
+++ src/types/events/base-events.ts
@@ -654,6 +654,7 @@ export interface UserChangeEvent {
     color: string;
     real_name: string;
     tz: string;
+    tz_label: string;
     tz_offset: number;
     profile: {
       title: string;
@@ -664,19 +665,22 @@ export interface UserChangeEvent {
       display_name: string;
       display_name_normalized: string;
       status_text: string;
+      status_text_canonical: string;
       status_emoji: string;
       status_expiration: number;
       avatar_hash: string;
       first_name: string;
       last_name: string;
       email: string;
-      image_original: string;
+      image_original?: string;
+      is_custom_image?: boolean;
       image_24: string;
       image_32: string;
       image_48: string;
       image_72: string;
       image_192: string;
       image_512: string;
+      image_1024?: string;
       team: string;
     },
     is_admin: boolean;
@@ -685,12 +689,21 @@ export interface UserChangeEvent {
     is_restricted: boolean;
     is_ultra_restricted: boolean;
     is_bot: boolean;
-    is_stranger: boolean;
+    is_stranger?: boolean;
     updated: string;
+    is_email_confirmed: boolean;
     is_app_user: boolean;
-    is_invited_user: boolean;
-    has_2fa: boolean;
+    is_invited_user?: boolean;
+    has_2fa?: boolean;
     locale: string;
+    enterprise_user?: {
+      id: string;
+      enterprise_id: string;
+      enterprise_name: string;
+      is_admin: boolean;
+      is_owner: boolean;
+      teams: string[];    
+    };
   };
 }
 
