slackapi/bolt

Fix #859 Improve the way to handle authorize fn errors in built-in receivers

src/App.ts
@@ -48,7 +48,7 @@ import {
   RespondArguments,
 } from './types';
 import { IncomingEventType, getTypeAndConversation, assertNever } from './helpers';
-import { CodedError, asCodedError, AppInitializationError, MultipleListenerError } from './errors';
+import { CodedError, asCodedError, AppInitializationError, MultipleListenerError, ErrorCode } from './errors';
 // eslint-disable-next-line import/order
 import allSettled = require('promise.allsettled'); // eslint-disable-line @typescript-eslint/no-require-imports
 // eslint-disable-next-line @typescript-eslint/no-require-imports
@@ -673,7 +673,7 @@ export default class App {
       }
     } catch (error) {
       this.logger.warn('Authorization of incoming event did not succeed. No listeners will be called.');
-      error.code = 'slack_bolt_authorization_error';
+      error.code = ErrorCode.AuthorizationError;
       return this.handleError(error);
     }
 
@@ -870,7 +870,7 @@ const tokenUsage =
   'should be initialized with oauth installer or authorize.';
 
 function defaultErrorHandler(logger: Logger): ErrorHandler {
-  return (error) => {
+  return (error: CodedError) => {
     logger.error(error);
 
     return Promise.reject(error);

slackapi/bolt-js

src/App.ts
@@ -55,7 +55,7 @@ import {
   WorkflowStepEdit,
 } from './types';
 import { IncomingEventType, getTypeAndConversation, assertNever } from './helpers';
-import { CodedError, asCodedError, AppInitializationError, MultipleListenerError } from './errors';
+import { CodedError, asCodedError, AppInitializationError, MultipleListenerError, ErrorCode } from './errors';
 // eslint-disable-next-line import/order
 import allSettled = require('promise.allsettled'); // eslint-disable-line @typescript-eslint/no-require-imports
 // eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-commonjs
@@ -692,7 +692,7 @@ export default class App {
     } catch (error) {
       const e = error as any;
       this.logger.warn('Authorization of incoming event did not succeed. No listeners will be called.');
-      e.code = 'slack_bolt_authorization_error';
+      e.code = ErrorCode.AuthorizationError;
       // disabling due to https://github.com/typescript-eslint/typescript-eslint/issues/1277
       // eslint-disable-next-line consistent-return
       return this.handleError(e);
@@ -907,7 +907,7 @@ export default class App {
 }
 
 function defaultErrorHandler(logger: Logger): ErrorHandler {
-  return (error) => {
+  return (error: CodedError) => {
     logger.error(error);
 
     return Promise.reject(error);