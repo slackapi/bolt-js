9a7313fbd24c08af236584404b2bf2836afe015c
Sarah Jiang <sarahjiang@slack-corp.com>
2022-06-07 09:28:24 -0700

    Add manifest sample

--- src/cli/manifest.js
+++ src/cli/manifest.js
@@ -2,16 +2,42 @@
 // Manifest script hook returns a manifest JSON if it exists in the working directory
 const fs = require('fs');
 const path = require('path');
+const fileName = 'manifest';
+const fileFormats = {
+  JSON: 'json',
+  js: 'js',
+  ts: 'ts'  
+};
+
 (function _(cwd) {
-  // TODO: Support additonal accepted formats
-  // const acceptedFormat = ['json', 'yml', 'ts'];
-  let fileData;
-  try {
-    fileData = fs.readFileSync(path.resolve(cwd, 'manifest.json'), 'utf8');
-  } catch (error) {
-    // TODO: Throw a coded error
-    console.error(`Failed to find a manifest file in this project: ${error}`);
-    process.exit(1);
-  }
-  console.log(JSON.stringify(JSON.parse(fileData)));
+  // Looks for a manifest.json file. If exists, use it.
+  // Looks for a manifest.ts file. If exists, default export is used. If you also had a manifest.json file, 
+  // it is deep-merged on top of the json file.
+  // If no manifest.ts exists, looks for a manifest.js file, and follows the same logic as manifest.ts does.
+  let manifest;
+ 
+    try {
+      // look for a manifest.json
+      manifest = getJSON(cwd);
+      if (manifest === null) {
+        // look for a manifest.js or manifest.ts
+        console.log('there is no manifest.json');ejbbccujfefbfjncvttecgvfuvlvecnhlihenijddibg
+      }
+    } catch (error) {
+      // TODO: Throw a coded error
+      console.error(`Failed to find a manifest file in this project: ${error}`);
+      // process.exit(1);
+    }
+  console.log(manifest);
 }(process.cwd()));
+
+const getJSON = (cwd) => {
+  let fileDataJSON;
+  try {
+    fileDataJSON = fs.readFileSync(path.resolve(cwd, `${fileName}.${fileFormats.JSON}`), 'utf8');
+    console.log(`Found manifest.json file in this project`);
+  } catch (error) {
+    return null;
+  }
+  return JSON.stringify(JSON.parse(fileDataJSON));
+}
