53bb9929686fbb8f6763ed83533c02077caab657
Miriam Lauter <lauter.miriam@gmail.com>
2023-05-24 19:33:09 -0400

    Fix #1512 Remove say from SlackShortcutMiddlewareArgs for GlobalSho… (#1849)
    * Fixes 1512: Remove say from SlackShortcutMiddlewareArgs for GlobalShortcuts
    
    * Fix comment in test
    
    ---------
    
    Co-authored-by: Kazuhiro Sera <seratch@gmail.com>

--- types-tests/shortcut.test-d.ts
+++ types-tests/shortcut.test-d.ts
@@ -1,5 +1,5 @@
 import { expectError, expectType } from 'tsd';
-import { App, MessageShortcut } from '../';
+import { App, GlobalShortcut, MessageShortcut, SayFn } from '../';
 
 const app = new App({ token: 'TOKEN', signingSecret: 'Signing Secret' });
 
@@ -17,5 +17,22 @@ expectType<void>(app.shortcut({ type: 'message_action' }, async ({ shortcut }) =
 // If shortcut is parameterized with MessageShortcut, shortcut argument in callback should be type MessageShortcut
 expectType<void>(app.shortcut<MessageShortcut>({}, async ({ shortcut }) => {
   expectType<MessageShortcut>(shortcut);
+  await Promise.resolve(shortcut);
+}));
+
+expectType<void>(app.shortcut({}, async ({ shortcut, say }) => {
+  expectType<SayFn | undefined>(say);
+  await Promise.resolve(shortcut);
+}));
+
+// If shortcut is parameterized with MessageShortcut, say argument in callback should be type SayFn
+expectType<void>(app.shortcut<MessageShortcut>({}, async ({ shortcut, say }) => {
+  expectType<SayFn>(say);
+  await Promise.resolve(shortcut);
+}));
+
+// If shortcut is parameterized with GlobalShortcut, say argument in callback should be type undefined
+expectType<void>(app.shortcut<GlobalShortcut>({}, async ({ shortcut, say }) => {
+  expectType<undefined>(say);
   await Promise.resolve(shortcut);
 }));
