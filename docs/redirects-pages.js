37b33424430a1f4bb3f89bbc16d70f31399f0699
Luke Russell <luke.russell@slack-corp.com>
2024-07-31 12:19:13 -0700

    docs: added japanese redirects

--- docs/redirects-pages.js
+++ docs/redirects-pages.js
@@ -4,10 +4,14 @@ if (ExecutionEnvironment.canUseDOM) {
   (function() {
     // List of specific paths to redirect
     const redirects = {
+      '/bolt-js/': '/bolt-js/getting-started',
       '/bolt-js/concepts/basic': '/bolt-js/getting-started',
       '/bolt-js/concepts/advanced': '/bolt-js/getting-started',
-      '/bolt-js/': '/bolt-js/getting-started',
       '/bolt-js/tutorial/getting-started': '/bolt-js/getting-started',
+      '/bolt-js/ja-jp/': '/bolt-js/ja-jp/getting-started',
+      '/bolt-js/ja-jp/concepts/basic': '/bolt-js/ja-jp/getting-started',
+      '/bolt-js/ja-jp/concepts/advanced': '/bolt-js/ja-jp/getting-started',
+      '/bolt-js/ja-jp/tutorial/getting-started': '/bolt-js/ja-jp/getting-started',
     };
 
     // Get the current path
