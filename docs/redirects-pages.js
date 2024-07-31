7160477ce4d8a681693bf96130367d4d4bda7ad8
Luke Russell <luke.russell@slack-corp.com>
2024-07-31 12:29:13 -0700

    docs - formatting for redirects-pages

--- docs/redirects-pages.js
+++ docs/redirects-pages.js
@@ -1,17 +1,17 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 if (ExecutionEnvironment.canUseDOM) {
-  (function() {
+  (function () {
     // List of specific paths to redirect
     const redirects = {
-      '/bolt-js/': '/bolt-js/getting-started',
-      '/bolt-js/concepts/basic': '/bolt-js/getting-started',
-      '/bolt-js/concepts/advanced': '/bolt-js/getting-started',
-      '/bolt-js/tutorial/getting-started': '/bolt-js/getting-started',
-      '/bolt-js/ja-jp/': '/bolt-js/ja-jp/getting-started',
-      '/bolt-js/ja-jp/concepts/basic': '/bolt-js/ja-jp/getting-started',
-      '/bolt-js/ja-jp/concepts/advanced': '/bolt-js/ja-jp/getting-started',
-      '/bolt-js/ja-jp/tutorial/getting-started': '/bolt-js/ja-jp/getting-started',
+      "/bolt-js/": "/bolt-js/getting-started",
+      "/bolt-js/concepts/basic": "/bolt-js/getting-started",
+      "/bolt-js/concepts/advanced": "/bolt-js/getting-started",
+      "/bolt-js/tutorial/getting-started": "/bolt-js/getting-started",
+      "/bolt-js/ja-jp/": "/bolt-js/ja-jp/getting-started",
+      "/bolt-js/ja-jp/concepts/basic": "/bolt-js/ja-jp/getting-started",
+      "/bolt-js/ja-jp/concepts/advanced": "/bolt-js/ja-jp/getting-started",
+      "/bolt-js/ja-jp/tutorial/getting-started": "/bolt-js/ja-jp/getting-started",
     };
 
     // Get the current path
