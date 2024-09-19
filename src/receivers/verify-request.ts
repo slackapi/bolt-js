540875b724de583cb01ebe2cb08459cc0eb503c1
Filip Maj <fmaj@slack-corp.com>
2024-09-19 15:20:56 -0700

    breaking change: remove deprecated verify method

--- src/receivers/verify-request.ts
+++ src/receivers/verify-request.ts
@@ -1,10 +1,6 @@
-// Deprecated: this function will be removed in the near future. Use HTTPModuleFunctions instead.
 import { createHmac } from 'crypto';
-import type { IncomingMessage, ServerResponse } from 'http';
-import { ConsoleLogger, Logger } from '@slack/logger';
+import { Logger } from '@slack/logger';
 import tsscmp from 'tsscmp';
-import { BufferedIncomingMessage } from './BufferedIncomingMessage';
-import { HTTPModuleFunctions, RequestVerificationOptions } from './HTTPModuleFunctions';
 
 // ------------------------------
 // HTTP module independent methods
@@ -79,28 +75,4 @@ export function isValidSlackRequest(options: SlackRequestVerificationOptions): b
     }
   }
   return false;
-}
-
-// ------------------------------
-// legacy methods (deprecated)
-// ------------------------------
-
-const consoleLogger = new ConsoleLogger();
-
-// Deprecated: this function will be removed in the near future. Use HTTPModuleFunctions instead.
-export interface VerifyOptions extends RequestVerificationOptions {
-  enabled?: boolean;
-  signingSecret: string;
-  nowMs?: () => number;
-  logger?: Logger;
-}
-
-// Deprecated: this function will be removed in the near future. Use HTTPModuleFunctions instead.
-export async function verify(
-  options: VerifyOptions,
-  req: IncomingMessage,
-  res?: ServerResponse,
-): Promise<BufferedIncomingMessage> {
-  consoleLogger.warn('This method is deprecated. Use HTTPModuleFunctions.parseAndVerifyHTTPRequest(options, req, res) instead.');
-  return HTTPModuleFunctions.parseAndVerifyHTTPRequest(options, req, res);
 }
