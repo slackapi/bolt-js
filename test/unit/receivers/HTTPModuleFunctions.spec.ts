ee2e1cbe9d6b4ffa02356da87ff71ed3bde4c6fa
Filip Maj <fmaj@slack-corp.com>
2024-09-24 16:09:55 -0400

    cleaning up HTTPModuleFunctions specs.

--- test/unit/receivers/HTTPModuleFunctions.spec.ts
+++ test/unit/receivers/HTTPModuleFunctions.spec.ts
@@ -1,42 +1,29 @@
-import 'mocha';
-import { createHmac } from 'crypto';
-import { IncomingMessage, ServerResponse } from 'http';
+import { createHmac } from 'node:crypto';
+import { IncomingMessage, ServerResponse } from 'node:http';
 import { assert } from 'chai';
 import sinon from 'sinon';
 
-import { AuthorizationError, HTTPReceiverDeferredRequestError, ReceiverMultipleAckError } from '../errors';
-import { createFakeLogger } from '../test-helpers';
-import type { BufferedIncomingMessage } from './BufferedIncomingMessage';
-import { HTTPModuleFunctions as func } from './HTTPModuleFunctions';
+import { AuthorizationError, HTTPReceiverDeferredRequestError, ReceiverMultipleAckError } from '../../../src/errors';
+import { createFakeLogger } from '../helpers';
+import type { BufferedIncomingMessage } from '../../../src/receivers/BufferedIncomingMessage';
+import { HTTPModuleFunctions as func } from '../../../src/receivers/HTTPModuleFunctions';
 
 describe('HTTPModuleFunctions', async () => {
   describe('Request header extraction', async () => {
     describe('extractRetryNumFromHTTPRequest', async () => {
       it('should work when the header does not exist', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         const result = func.extractRetryNumFromHTTPRequest(req);
         assert.isUndefined(result);
       });
       it('should parse a single value header', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         req.headers['x-slack-retry-num'] = '2';
         const result = func.extractRetryNumFromHTTPRequest(req);
         assert.equal(result, 2);
       });
       it('should parse an array of value headers', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         req.headers['x-slack-retry-num'] = ['2'];
         const result = func.extractRetryNumFromHTTPRequest(req);
         assert.equal(result, 2);
@@ -45,29 +32,17 @@ describe('HTTPModuleFunctions', async () => {
     describe('extractRetryReasonFromHTTPRequest', async () => {
       it('should work when the header does not exist', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         const result = func.extractRetryReasonFromHTTPRequest(req);
         assert.isUndefined(result);
       });
       it('should parse a valid header', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         req.headers['x-slack-retry-reason'] = 'timeout';
         const result = func.extractRetryReasonFromHTTPRequest(req);
         assert.equal(result, 'timeout');
       });
       it('should parse an array of value headers', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         req.headers['x-slack-retry-reason'] = ['timeout'];
         const result = func.extractRetryReasonFromHTTPRequest(req);
         assert.equal(result, 'timeout');
@@ -98,23 +73,15 @@ describe('HTTPModuleFunctions', async () => {
     describe('getHeader', async () => {
       it('should throw an exception when parsing a missing header', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         try {
           func.getHeader(req, 'Cookie');
           assert.fail('Error should be thrown here');
         } catch (e) {
-          assert.isTrue((e as any).message.length > 0);
+          assert.isTrue((e as Error).message.length > 0);
         }
       });
       it('should parse a valid header', async () => {
         const req = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        if (req.headers === undefined) {
-          // sinon on older Node.js may not return an object here
-          req.headers = {};
-        }
         req.headers.Cookie = 'foo=bar';
         const result = func.getHeader(req, 'Cookie');
         assert.equal(result, 'foo=bar');
@@ -137,7 +104,7 @@ describe('HTTPModuleFunctions', async () => {
             'x-slack-request-timestamp': timestamp,
           },
         } as unknown as BufferedIncomingMessage;
-        const res: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
+        const res = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
         const result = await func.parseAndVerifyHTTPRequest({ signingSecret }, req, res);
         assert.isDefined(result.rawBody);
       });
@@ -160,8 +127,9 @@ describe('HTTPModuleFunctions', async () => {
         try {
           await func.parseAndVerifyHTTPRequest({ signingSecret }, req, res);
         } catch (e) {
-          assert.equal(
-            (e as any).message,
+          assert.propertyVal(
+            e,
+            'message',
             'Failed to verify authenticity: x-slack-request-timestamp must differ from system time by no more than 5 minutes or request is stale',
           );
         }
@@ -182,7 +150,7 @@ describe('HTTPModuleFunctions', async () => {
         try {
           await func.parseAndVerifyHTTPRequest({ signingSecret }, req, res);
         } catch (e) {
-          assert.equal((e as any).message, 'Failed to verify authenticity: signature mismatch');
+          assert.propertyVal(e, 'message', 'Failed to verify authenticity: signature mismatch');
         }
       });
       it('should parse a ssl_check request body without signature verification', async () => {
@@ -214,7 +182,7 @@ describe('HTTPModuleFunctions', async () => {
         try {
           await func.parseAndVerifyHTTPRequest({ signingSecret }, req, res);
         } catch (e) {
-          assert.equal((e as any).message, 'Failed to verify authenticity: signature mismatch');
+          assert.propertyVal(e, 'message', 'Failed to verify authenticity: signature mismatch');
         }
       });
     });
@@ -222,32 +190,26 @@ describe('HTTPModuleFunctions', async () => {
 
   describe('HTTP response builder methods', async () => {
     it('should have buildContentResponse', async () => {
-      const res: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-      const writeHead = sinon.fake();
-      res.writeHead = writeHead;
-      func.buildContentResponse(res, 'OK');
-      assert.isTrue(writeHead.calledWith(200));
+      const res = sinon.createStubInstance(ServerResponse);
+      func.buildContentResponse(res as unknown as ServerResponse, 'OK');
+      assert.isTrue(res.writeHead.calledWith(200));
     });
     it('should have buildNoBodyResponse', async () => {
-      const res: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-      const writeHead = sinon.fake();
-      res.writeHead = writeHead;
-      func.buildNoBodyResponse(res, 500);
-      assert.isTrue(writeHead.calledWith(500));
+      const res = sinon.createStubInstance(ServerResponse);
+      func.buildNoBodyResponse(res as unknown as ServerResponse, 500);
+      assert.isTrue(res.writeHead.calledWith(500));
     });
     it('should have buildSSLCheckResponse', async () => {
-      const res: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-      const writeHead = sinon.fake();
-      res.writeHead = writeHead;
-      func.buildSSLCheckResponse(res);
-      assert.isTrue(writeHead.calledWith(200));
+      const res = sinon.createStubInstance(ServerResponse);
+      func.buildSSLCheckResponse(res as unknown as ServerResponse);
+      assert.isTrue(res.writeHead.calledWith(200));
     });
     it('should have buildUrlVerificationResponse', async () => {
-      const res: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-      const writeHead = sinon.fake();
-      res.writeHead = writeHead;
-      func.buildUrlVerificationResponse(res, { challenge: '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P' });
-      assert.isTrue(writeHead.calledWith(200));
+      const res = sinon.createStubInstance(ServerResponse);
+      func.buildUrlVerificationResponse(res as unknown as ServerResponse, {
+        challenge: '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P',
+      });
+      assert.isTrue(res.writeHead.calledWith(200));
     });
   });
 
@@ -257,75 +219,65 @@ describe('HTTPModuleFunctions', async () => {
     describe('defaultDispatchErrorHandler', async () => {
       it('should properly handle ReceiverMultipleAckError', async () => {
         const request = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        const response: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-        const writeHead = sinon.fake();
-        response.writeHead = writeHead;
+        const response = sinon.createStubInstance(ServerResponse);
         func.defaultDispatchErrorHandler({
           error: new ReceiverMultipleAckError(),
           logger,
           request,
-          response,
+          response: response as unknown as ServerResponse,
         });
-        assert.isTrue(writeHead.calledWith(500));
+        assert.isTrue(response.writeHead.calledWith(500));
       });
       it('should properly handle HTTPReceiverDeferredRequestError', async () => {
         const request = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        const response: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-        const writeHead = sinon.fake();
-        response.writeHead = writeHead;
+        const response = sinon.createStubInstance(ServerResponse);
         func.defaultDispatchErrorHandler({
-          error: new HTTPReceiverDeferredRequestError('msg', request, response),
+          error: new HTTPReceiverDeferredRequestError('msg', request, response as unknown as ServerResponse),
           logger,
           request,
-          response,
+          response: response as unknown as ServerResponse,
         });
-        assert.isTrue(writeHead.calledWith(404));
+        assert.isTrue(response.writeHead.calledWith(404));
       });
     });
 
     describe('defaultProcessEventErrorHandler', async () => {
       it('should properly handle ReceiverMultipleAckError', async () => {
         const request = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        const response: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-        const writeHead = sinon.fake();
-        response.writeHead = writeHead;
+        const response = sinon.createStubInstance(ServerResponse);
         func.defaultProcessEventErrorHandler({
           error: new ReceiverMultipleAckError(),
           storedResponse: undefined,
           logger,
           request,
-          response,
+          response: response as unknown as ServerResponse,
         });
-        assert.isTrue(writeHead.calledWith(500));
+        assert.isTrue(response.writeHead.calledWith(500));
       });
       it('should properly handle AuthorizationError', async () => {
         const request = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        const response: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-        const writeHead = sinon.fake();
-        response.writeHead = writeHead;
+        const response = sinon.createStubInstance(ServerResponse);
         func.defaultProcessEventErrorHandler({
           error: new AuthorizationError('msg', new Error()),
           storedResponse: undefined,
           logger,
           request,
-          response,
+          response: response as unknown as ServerResponse,
         });
-        assert.isTrue(writeHead.calledWith(401));
+        assert.isTrue(response.writeHead.calledWith(401));
       });
     });
 
     describe('defaultUnhandledRequestHandler', async () => {
       it('should properly execute', async () => {
         const request = sinon.createStubInstance(IncomingMessage) as IncomingMessage;
-        const response: ServerResponse = sinon.createStubInstance(ServerResponse) as unknown as ServerResponse;
-        const writeHead = sinon.fake();
-        response.writeHead = writeHead;
+        const response = sinon.createStubInstance(ServerResponse);
         func.defaultUnhandledRequestHandler({
           logger,
           request,
-          response,
+          response: response as unknown as ServerResponse,
         });
-        assert.isTrue(writeHead.calledWith(404));
+        assert.isTrue(response.writeHead.calledWith(404));
       });
     });
   });
