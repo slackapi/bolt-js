ba1f91b0b8a9007997157a07af2fbd6ea8d4e7b1
Filip Maj <fmaj@slack-corp.com>
2024-09-27 11:32:29 -0400

    adding some type tests for `action` handlers re: `say` utility

--- test/types/action.test-d.ts
+++ test/types/action.test-d.ts
@@ -1,38 +1,50 @@
-import { expectAssignable, expectError, expectType } from 'tsd';
-import type { BlockElementAction, DialogSubmitAction, InteractiveAction, SlackAction } from '../../';
-import App from '../../src/App';
+import { expectAssignable, expectError, expectType } from "tsd";
+import type {
+  BlockElementAction,
+  DialogSubmitAction,
+  InteractiveAction,
+  SayFn,
+  SlackAction,
+} from "../../";
+import App from "../../src/App";
 
-const app = new App({ token: 'TOKEN', signingSecret: 'Signing Secret' });
+const app = new App({ token: "TOKEN", signingSecret: "Signing Secret" });
 
-// calling action method with incorrect an type constraint value should not work
+// calling action method with incorrect type constraint value should not work
 expectError(
-  app.action({ type: 'Something wrong' }, async ({ action }) => {
+  app.action({ type: "Something wrong" }, async ({ action }) => {
     await Promise.resolve(action);
   }),
 );
 
-app.action({ type: 'block_actions' }, async ({ action }) => {
+app.action({ type: "block_actions" }, async ({ action, say }) => {
   expectType<BlockElementAction>(action);
+  expectType<SayFn>(say);
 });
 
-app.action({ type: 'interactive_message' }, async ({ action }) => {
+app.action({ type: "interactive_message" }, async ({ action, say }) => {
   expectType<InteractiveAction>(action);
+  expectType<SayFn>(say);
 });
 
-app.action({ type: 'dialog_submission' }, async ({ action }) => {
+app.action({ type: "dialog_submission" }, async ({ action }) => {
   expectType<DialogSubmitAction>(action);
 });
 
+expectError(
+  app.action({ type: "dialog_submission" }, async ({ say }) => say()),
+);
+
 interface MyContext {
-  doesnt: 'matter';
+  doesnt: "matter";
 }
 // Ensure custom context assigned to individual middleware is honoured
-app.action<SlackAction, MyContext>('action_id', async ({ context }) => {
+app.action<SlackAction, MyContext>("action_id", async ({ context }) => {
   expectAssignable<MyContext>(context);
 });
 
 // Ensure custom context assigned to the entire app is honoured
 const typedContextApp = new App<MyContext>();
-typedContextApp.action('action_id', async ({ context }) => {
+typedContextApp.action("action_id", async ({ context }) => {
   expectAssignable<MyContext>(context);
 });
